180d01aff0d5a557ce72d751d5d171c4764271e3
KLan Stovall <kland13@gmail.com>
2022-01-12 22:04:30 -0800

    lab complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Django REST
+
+Created an API with the REST framework
---
+++ snacks/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Snack
+from .serializers import SnackSerializer
+
+class SnackList(generics.ListCreateAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+
+class SnackDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
